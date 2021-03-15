d42fccc1eff68c097682b94a6eeef69c5001f186
Irina Rusinova <rusinovarusya@mail.ru>
2021-03-15 21:45:10 +0300

    fix: fix part of  errors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ module.exports = function check(str, bracketsConfig) {
   for (let key of str) {
     let kind = kindOfBrackets(key);
     if (kind > -1 && kind < 3) stack.push(kind);
-    else {
+    if (kind < 1) {
       if (stack.length == 0 || kind + stack.pop() != 0) return false;
     }
   }
