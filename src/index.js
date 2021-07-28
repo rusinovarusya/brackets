9c0200f10dc3c884b4b0e3399a3d35e7315b7b75
Irina Rusinova <rusinovarusya@mail.ru>
2021-07-28 08:36:36 +0300

    feat: do 18/20 of task

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  let kindOfBrackets = (bracket) => {
-    switch (bracket) {
-      case '(': return 1;
-      case ')': return -1;
-      case '[': return 2;
-      case ']': return -2;
-      case '|': return 0;
-      default: 3;
+    let kindOfBrackets = (bracket) => {
+        switch (bracket) {
+            case '(': return 1;
+            case ')': return -1;
+            case '[': return 2;
+            case ']': return -2;
+            case '|': return 0;
+            default: 3;
+        }
     }
-  }
-  
-  let stack = [];
-  for (let key of str) {
-    let kind = kindOfBrackets(key);
-    if (kind > -1 && kind < 3) stack.push(kind);
-    if (kind < 1) {
-      if (stack.length == 0 || kind + stack.pop() != 0) return false;
-    }
-  }
-  if (stack.length != 0) return false;
-  return true;
 
+    let stack = [];
+    for (let key of str) {
+        let kind = kindOfBrackets(key);
+        if (kind > 0 && kind < 3) stack.push(kind);
+        if (kind < 0) {
+            if (stack.length == 0 || (kind + stack.pop()) != 0) return false;
+        }
+        if (kind == 0) {
+            if (stack[stack.length - 1] == kind) stack.pop();
+            else stack.push(kind);
+        }
+    }
+    if (stack.length != 0) return false;
+    return true;
 }
